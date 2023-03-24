4bdacd2749685fd141523dc39815de1534d1bd0b
Oicnanev <voicnanev@proton.me>
2023-03-24 22:31:53 +0000

    aula

--- hex.pl
+++ hex.pl
@@ -1,2 +1,34 @@
 % Define the size of the board
 board_size(11).
+% size(L) = read(L).
+
+% players definitions
+black(X) :- X = '\u2b22'.
+white(X) :- X = '\u2b21'.
+
+% Define the board
+createBoard(B) :-
+    B = [['.','.','.'],
+         ['.','.','.'],
+         ['.','.','.']].
+
+
+printBoard([]) :- nl.
+printBoard([H|T]) :-
+    printLine(H),
+    printBoard(T).
+
+% TODO - implement this
+% play(Board, Player, NewBoard) :-
+
+printLine([]) :-
+    nl.
+printLine([H|T]) :-
+    write(H),write(' '),
+    printLine(T).
+
+% ?- createBoard(B), printBoard(B).
+
+printTab(Space) :-
+    atom_concat(' ', Space, NewSpace).
+
