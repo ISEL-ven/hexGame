faed5d8138aa6d03f8fd0970ad1fd6ce26f068ea
Oicnanev <voicnanev@proton.me>
2023-03-31 19:48:01 +0100

    getElem_nth1 and next_playe

--- hex.pl
+++ hex.pl
@@ -3,8 +3,14 @@ board_size(11).
 % size(L) = read(L).
 
 % players definitions
-black(X) :- X = '\u2b22'.
-white(X) :- X = '\u2b21'.
+player(black) :- '\u2b22'.
+player(white) :- '\u2b21'.
+%black(X) :- X = '\u2b22'.
+%white(X) :- X = '\u2b21'.
+
+% next player
+next_player(black, white).
+next_player(white, black).
 
 % Define the board
 createBoard(B) :-
@@ -18,6 +24,10 @@ printBoard([H|T]) :-
     printLine(H),
     printBoard(T).
 
+getElem_nth1(Board, RowIndex, ColIndex, Elem) :-
+    nth1(RowIndex, Board, Row),
+    nth1(ColIndex, Row, Elem)
+
 % TODO - implement this
 % play(Board, Player, NewBoard) :-
 
@@ -27,8 +37,19 @@ printLine([H|T]) :-
     write(H),write(' '),
     printLine(T).
 
+
+% TODO: implement checkVictory()
+
+% TODO: implement moves() usando o find-all
+
+% DEBUG AND TESTS ---------------------------------------------
 % ?- createBoard(B), printBoard(B).
 
 printTab(Space) :-
     atom_concat(' ', Space, NewSpace).
+
+
+% function tab makes a pyamid with * and spaces
+pyamid(N):-
+    pyamid(N, 1).
 
